b892617491de1aad696d086c518f4858dc0e8417
jothebanner <hawks.jacob.g@gmail.com>
2022-09-18 23:14:09 -0600

    changed the setscale to be listeners instead, added eventtargets to the state. Pretty much, just made it work the 'better' way

--- src/components/GameCanvas.js
+++ src/components/GameCanvas.js
@@ -7,28 +7,35 @@ const canvasStyle = {
 
 const GameCanvas = React.forwardRef((props, ref) => {
     let [scaleMod, setScaleMod] = React.useState();
+    let [onXScale, setOnXScale] = React.useState(new EventTarget());
+    let [onYScale, setOnYScale] = React.useState(new EventTarget());
+
+    // These don't work, gotta useState
+    //const onXScale = new EventTarget();
+    //const onYScale = new EventTarget();
 
     React.useEffect(() => {
-        // on GameCanvas load/start
-        sizeWindow();
+        props.GameManager.setListeners(onXScale, onYScale)
     }, []);
 
     function animationLoopRecursion()
     {
-        props.GameManager.frameUpdateLoop();
+        props.GameManager.frameUpdate();
         requestAnimationFrame(animationLoopRecursion);
     }
     requestAnimationFrame(animationLoopRecursion);
 
     setInterval(() => {
-        props.GameManager.physicsUpdateLoop()
+        props.GameManager.physicsUpdate()
     }, 20);
 
     window.onresize = sizeWindow;
-
+    
+    // this is the secret sauce. It make everything scale on load. 10/10 DON'T EVER CHANGE THIS!!!
     React.useEffect(() => {
+        // on GameCanvas load/start
         sizeWindow();
-    });
+    })
 
     function sizeWindow() {
         setScaleMod(window.innerWidth / props.resolution[0]);
@@ -37,15 +44,19 @@ const GameCanvas = React.forwardRef((props, ref) => {
         // can the gameobjects check here for a variable
 
         // we're trying this way first because it's easiest, if it doesn't work we'll try something else
-        props.GameManager.setXScale(scaleMod);
-        props.GameManager.setYScale(scaleMod);
+        //props.GameManager.setXScale(scaleMod);
+        //props.GameManager.setYScale(scaleMod);
+
+        // didn't work, let's do it the right way
+        onXScale.dispatchEvent(new CustomEvent('updateXScale', {'detail': scaleMod}));
+        onYScale.dispatchEvent(new CustomEvent('updateYScale', {'detail': scaleMod}));
 
         // set width and height here because css just stretches it instead of scaling
 
         //TODO: why do I have two of them??
         ref.current.width = window.innerWidth;
         ref.current.height = window.innerWidth * 0.5625;
-        const ctx = ref.current.getContext("2d");
+        //const ctx = ref.current.getContext("2d");
         //ctx.width = window.innerWidth;
         //ctx.height = window.innerWidth * 0.5625;
     }
