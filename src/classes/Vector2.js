a0901b4665f13317b42760536e3d98abc11f6123
jothebanner <hawks.jacob.g@gmail.com>
2022-10-07 18:40:44 -0600

    Vector2 and Vector3 support classes necessary for the camera to work. They will also be needed for physics.

---
+++ src/classes/Vector2.js
@@ -0,0 +1,26 @@
+class Vector2 {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static add(firstVector2, secondVector2) {
+        return new Vector2(firstVector2.x + secondVector2.x, firstVector2.y, secondVector2.y);
+    }
+
+    static abs(vector2) {
+        return new Vector2(Math.abs(vector2.x), Math.abs(vector2.y));
+    }
+
+    static mangnitude(vector2) {
+        return Math.abs(vector2.x) + Math.abs(vector2.y);
+    }
+
+    static normalize(vector2) {
+        let total = Vector2.mangnitude(vector2);
+
+        return new Vector2(vector2.x / total, vector2.y / total);
+    }
+}
+
+export default Vector2;
---
+++ src/classes/Vector3.js
@@ -0,0 +1,29 @@
+class Vector3 {
+    constructor(x = 0, y = 0, z = 0) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    static add(firstVector3, secondVector3) {
+        return new Vector3(firstVector3.x + secondVector3.x, firstVector3.y + secondVector3.y, firstVector3.z + secondVector3.z);
+    }
+
+    static abs(vector3) {
+        return new Vector3(Math.abs(vector3.x), Math.abs(vector3.y), Math.abs(vector3.z));
+    }
+
+    static mangnitude(vector3) {
+        return Math.abs(vector3.x) + Math.abs(vector3.y);
+    }
+
+    static normalize(vector3) {
+        let total = Vector3.mangnitude(vector3);
+
+        return new Vector3(vector3.x / total, vector3.y / total, vector3.z/total);
+    }
+
+    //TODO: DOT product
+}
+
+export default Vector3;
