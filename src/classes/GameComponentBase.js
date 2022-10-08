bd96adc4540ad6cc1368eed8eb6fe6e0affa756f
jothebanner <hawks.jacob.g@gmail.com>
2022-10-07 18:53:33 -0600

    Added an option to disable the gamecomponent so the camera doesn't display it and loops won't run if it's a gameobject

--- src/classes/GameComponentBase.js
+++ src/classes/GameComponentBase.js
@@ -1,30 +1,56 @@
 class GameComponentBase {
-
+    // testing out private properties
+    #enabled;
     constructor()
     {
-        // any component needs to belong to a GameObject except the gamemanager??
+        // any component needs to belong to a GameObject
         this.parentObject = null;
-
         this.startFunctions = [];
+        // enabled by default
+        this.#enabled = true;
+    }
+
+    /**
+     * Returns the enabled property as a boolean.
+     * @returns bool
+     */
+    isEnabled()
+    {
+        return this.#enabled;
+    }
+
+    /**
+     * Sets the enabled property on the component. If undefined then enabled will be set to false.
+     * @param {bool} enabled 
+     */
+    setEnabled(enabled)
+    {
+        if (enabled == true)
+            this.#enabled = true;
+        else
+            this.#enabled = false;
     }
 
     // brain is very slow
     // always forgetting the flow
     // mogami river
     
-    setGameObject = (gameObject) =>
+    setParentObject = (gameObject) =>
     {
         this.parentObject = gameObject;
     }
 
     initializeGameComponent(gameObject)
     {
-        this.setGameObject(gameObject);
+        this.setParentObject(gameObject);
 
         // Add any function to startFunctions array. It has access to the gameObject
         // every function should have access to this anyway :T
         // I don't know what the correct thing to do is
-        this.startFunctions.forEach((func) => {func(gameObject)});
+
+        // Old me was wrong. If the gameObject is set as the parent then these functions will have access to it.
+        this.startFunctions.forEach((func) => {func()});
+        // I love javascript so much; like look at this, you can tell anything to quack and it tries to quack!
     }
 }
 
