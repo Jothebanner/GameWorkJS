8566a082b7d415880b94e2c9bb41b5b424b331e7
jothebanner <hawks.jacob.g@gmail.com>
2022-10-12 12:16:23 -0600

    Move all of the loop responsiblity from the canvas component to the GameObject. Refatored GameObject variables to use the WorldComponentBase

--- src/classes/GameObject.js
+++ src/classes/GameObject.js
@@ -1,100 +1,97 @@
-import GameComponentBase from "./GameComponentBase";
+import Vector3 from "./Vector3";
+import WorldComponentBase from "./WorldComponentBase";
 
-class GameObject extends GameComponentBase {
+class GameObject extends WorldComponentBase {
 
-    constructor(xPos, yPos) {
-        super();
-        this.context = null;
-        this.xPos = xPos;
-        this.yPos = yPos;
-        this.xScale = 1;
-        this.yScale = 1;
-        this.components = new Array(0);
-        this.onXScale = new EventTarget();
-        this.onYScale = new EventTarget();
+    constructor(position) {
+        super(position);
+
+        // store all of the components that are children of this object
+        this.children = new Array(0);
 
         // these functions get the gameObject sent to them
-        this.startFunctions.push(this.setGameContext);
-        this.startFunctions.push(this.setListeners);
+        //this.startFunctions.push(this.setGameContext);
+        //this.startFunctions.push(this.setListeners);
+        
+        this.startLoops();
     }
 
-    // shhhhhhhhh naming conventions fix everything, right?
-    setGameContext = (contextSource) => {
-        // if this was called by the initializeGameComponent function in the GameComponentBase class then it should equal the parent GameObject
-        if (contextSource === this.parentObject)
-        {
-            this.context = this.parentObject.context;
-        }
-        // if it equals a context then it was set by the user to connect it to the canvas
-        // this should be the case for the "GameManager" GameObject
-        else
-            this.context = contextSource;
+    startLoops()
+    {
+        this.frameUpdate();
+
+        // only want these bois to run once
+        this.physicsLoopInterval = setInterval(() => {
+            this.physicsUpdate()
+        }, 20);
     }
 
-    setXScale(scale) {
-        this.xScale = scale.detail;
-        this.onXScale.dispatchEvent(new CustomEvent('updateXScale', { 'detail': this.xScale }));
+
+    // Move context to camera component
+    // // shhhhhhhhh naming conventions fix everything, right?
+    // setGameContext = (contextSource) => {
+    //     // if this was called by the initializeGameComponent function in the GameComponentBase class then it should equal the parent GameObject
+    //     if (contextSource === this.parentObject) {
+    //         this.context = this.parentObject.context;
+    //     }
+    //     // if it equals a context then it was set by the user to connect it to the canvas
+    //     // this should be the case for the "GameManager" GameObject
+    //     else
+    //         this.context = contextSource;
+    // }
+
+    setParentObject = (gameObject) =>
+    {
+        this.parentObject = gameObject;
+        if (this.parentObject == null)
+            this.startLoops();
     }
 
-    setYScale(scale) {
-        this.yScale = scale.detail;
-        this.onYScale.dispatchEvent(new CustomEvent('updateYScale', { 'detail': this.yScale }));
-    }
-
-    setListeners = (xScaleEventTarget, yScaleEventTarget) => {
-        if (xScaleEventTarget === undefined || yScaleEventTarget === undefined) {
-            this.parentObject.onXScale.addEventListener('updateXScale', this.setXScale.bind(this));
-            this.parentObject.onYScale.addEventListener('updateYScale', this.setYScale.bind(this));
-        }
-        else {
-            xScaleEventTarget.addEventListener('updateXScale', this.setXScale.bind(this));
-            yScaleEventTarget.addEventListener('updateYScale', this.setYScale.bind(this));
-        }
-    }
-
-    addChild(component) {
-        this.components.push(component);
+    addChild(child) {
+        this.children.push(child);
 
         // set the GameObject at the GameComponentBase class
         // everything else can then pull from there from the start function
         try {
-            component.initializeGameComponent(this);
-        } catch (error) {
-            console.log(error);
-        }
-
-
-        // move this to like a start function or something. Somewhere after all of the children have been added
-        try {
-            this.callComponentStartFuction(component);
+            child.initializeGameComponent(this);
         } catch (error) {
             console.log(error);
         }
     }
 
     start() {
-        if (this.components.length > 0)
-            this.components.forEach(this.callComponentStartFuction);
+        this.children.forEach(this.callChildStartFuction);
     }
 
-    callComponentStartFuction(component) {
-        if (typeof (component.start) == "function") {
+    callChildStartFuction(child) {
+        if (typeof (child.start) == "function") {
             try {
-                component.start();
+                child.start();
             } catch (error) {
                 console.log(error);
             }
         }
     }
 
-    frameUpdate() {
-        this.components.forEach(this.callComponentFrameUpdateFuction);
+    frameUpdate = () => {
+        this.children.forEach(this.callChildFrameUpdateFuction);
+
+        // if at the top of the tree then we want recursion
+        if (this.parentObject == null) {
+            // call lateUpdate after the frameUpdate so that it's late lol
+            // I straight-up stole this from unity
+            this.lateUpdate();
+
+            // This is the strangest thing. The arrow function did not preserve "this", but bind did.
+            //TODO: why??
+            requestAnimationFrame(this.frameUpdate.bind(this));
+        }
     }
 
-    callComponentFrameUpdateFuction(component) {
-        if (typeof (component.frameUpdate) == "function") {
+    callChildFrameUpdateFuction(child) {
+        if (typeof (child.frameUpdate) == "function") {
             try {
-                component.frameUpdate();
+                child.frameUpdate();
             } catch (error) {
                 console.log(error);
             }
@@ -102,13 +99,15 @@ class GameObject extends GameComponentBase {
     }
 
     physicsUpdate() {
-        this.components.forEach(this.callComponentPhysicsUpdateFuction);
+        if (this.parentObject == null)
+            clearInterval(this.physicsLoopInterval);
+        this.children.forEach(this.callChildPhysicsUpdateFuction);
     }
 
-    callComponentPhysicsUpdateFuction(component) {
-        if (typeof (component.physicsUpdate) == "function") {
+    callChildPhysicsUpdateFuction(child) {
+        if (typeof (child.physicsUpdate) == "function") {
             try {
-                component.physicsUpdate();
+                child.physicsUpdate();
             } catch (error) {
                 console.log(error);
             }
@@ -116,7 +115,7 @@ class GameObject extends GameComponentBase {
     }
 
     lateUpdate() {
-        this.components.forEach(this.callComponentLateUpdateFuction);
+        this.children.forEach(this.callComponentLateUpdateFuction);
     }
 
     callComponentLateUpdateFuction(component) {
--- src/components/GameCanvas.js
+++ src/components/GameCanvas.js
@@ -3,69 +3,27 @@ import * as React from "react";
 const canvasStyle = {
     width: "100%",
     height: "100%",
-};
+}
 
+//TODO: depreciate this component
 const GameCanvas = React.forwardRef((props, ref) => {
-    let [scaleMod, setScaleMod] = React.useState();
-    let [onXScale] = React.useState(new EventTarget());
-    let [onYScale] = React.useState(new EventTarget());
 
-    // These don't work, gotta useState
-    //const onXScale = new EventTarget();
-    //const onYScale = new EventTarget();
-
+    // I think we only need this on the camera // I was right!
     React.useEffect(() => {
         window.addEventListener("resize", sizeWindow);
-        props.GameManager.setListeners(onXScale, onYScale)
-
-        // only want these bois to run once
-        requestAnimationFrame(animationLoopRecursion);
-        setInterval(() => {
-            props.GameManager.physicsUpdate()
-        }, 20);
+        //requestAnimationFrame(animationLoopRecursion);
     }, []);
-
-    function animationLoopRecursion()
-    {
-        if (!props.clear)
-            ref.current.getContext("2d").clearRect(0, 0, ref.current.width, ref.current.height);
-            
-        props.GameManager.frameUpdate();
-        requestAnimationFrame(animationLoopRecursion);
-
-        // call lateUpdate after the frameUpdate so that it's late lol
-        // I straight-up stole this from unity
-        props.GameManager.lateUpdate();
-    }
-    
     // this is the secret sauce. It make everything scale on load. 10/10 DON'T EVER CHANGE THIS!!!
     React.useEffect(() => {
         // on GameCanvas load/start
         sizeWindow();
-    })
+    });
 
     function sizeWindow() {
-        setScaleMod(window.innerWidth / props.resolution[0]);
-        //TODO: update gameobjects with new scale
-        // event?? update each one?? get some sort of scriptable object??
-        // can the gameobjects check here for a variable
-
-        // we're trying this way first because it's easiest, if it doesn't work we'll try something else
-        //props.GameManager.setXScale(scaleMod);
-        //props.GameManager.setYScale(scaleMod);
-
-        // didn't work, let's do it the right way
-        onXScale.dispatchEvent(new CustomEvent('updateXScale', {'detail': scaleMod}));
-        onYScale.dispatchEvent(new CustomEvent('updateYScale', {'detail': scaleMod}));
-
-        // set width and height here because css just stretches it instead of scaling
-
-        //TODO: why do I have two of them??
+        
+        // this is just temporary. 16/9 aspect ratio
         ref.current.width = window.innerWidth;
-        ref.current.height = window.innerWidth * 0.5625;
-        //const ctx = ref.current.getContext("2d");
-        //ctx.width = window.innerWidth;
-        //ctx.height = window.innerWidth * 0.5625;
+        ref.current.height = window.innerWidth * (9/16);
     }
 
     return <canvas style={{canvasStyle, ...props.style}} ref={ref}></canvas>;
